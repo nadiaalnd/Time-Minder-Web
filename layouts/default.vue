864f989afaa90baf3ee886a745667ae99840993b
nadiaalnd <nadila2244@gmail.com>
2024-03-21 15:44:40 +0700

    Update

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen font-sans antialiased relative">
     <div class="relative">
       <div
-        class="absolute top-0 left-0 w-full h-[125vh] sm:h-[215vh] lg:h-[120vh] cover-gradient-2 sm:cover-gradient"
+        class="absolute top-0 left-0 w-full h-[125vh] sm:h-[215vh] lg:h-[130vh] cover-gradient-2 sm:cover-gradient"
       ></div>
       <BaseNavbar class="sticky top-0 z-10" />
 
