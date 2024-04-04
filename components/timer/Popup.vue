d94ba2f62dfdc84656c7435656244fb73d4c582e
hafisrabbani <hafis.gov@gmail.com>
2024-04-05 01:39:15 +0700

    Update

--- components/timer/Popup.vue
+++ components/timer/Popup.vue
@@ -4,11 +4,11 @@
     class="fixed inset-0 overflow-y-auto z-50 flex items-center justify-center bg-black bg-opacity-50">
     <div class="relative p-4 max-w-2xl w-full">
       <!-- Konten Modal -->
-      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+      <div class="relative bg-white rounded-lg shadow">
         <!-- Header Modal -->
         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-          <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-            Waktu Habis
+          <h3 class="text-xl font-semibold text-gray-900">
+            Notification
           </h3>
           <button
             type="button"
@@ -16,7 +16,7 @@
             @click="$emit('close')">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
               <path
-                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
             </svg>
             <span class="sr-only">Close modal</span>
@@ -25,7 +25,7 @@
         <!-- Isi Modal -->
         <div class="p-4 md:p-5 space-y-4 h-12">
           <p class="text-gray-600 dark:text-gray-400">
-            Waktu telah habis, silahkan istirahat sejenak.
+            Sorry, your time is up. Please manage your time wisely for future tasks.
           </p>
         </div>
         <!-- Footer Modal -->
@@ -34,7 +34,7 @@
             type="button"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             @click="$emit('close')">
-            OKE SUWON BOLO
+            Close
           </button>
         </div>
       </div>
