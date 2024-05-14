dfdebbf3ebce25df3533593a433705eddb86f716
nadiaalnd <nadila2244@gmail.com>
2024-05-14 19:46:30 +0700

    Update yg bisa di update

--- components/timer/custom/ModalCustom.vue
+++ components/timer/custom/ModalCustom.vue
@@ -2,17 +2,22 @@
   <div class="fixed z-10 inset-0 overflow-y-auto" role="dialog" aria-modal="true">
     <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
       <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+      <div class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        <button @click="close" class="absolute top-0 right-0 mt-2 mr-2 text-gray-800 py-2 px-4">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="m8 8l32 32M8 40L40 8"/>
+          </svg>
+        </button>
         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <h3 class="text-lg leading-6 font-medium text-gray-900">Tambah Waktumu Sendiri!</h3>
           <FormInput />
         </div>
-        <div class="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-          <button @click="apply" class="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-md">
-            Terapkan
-          </button>
-          <button @click="reset" class="border-2 hover:bg-gray-100 border-neutral-950 py-2 px-4 mr-2 rounded-md">
+        <div class="px-4 py-3 sm:px-6 flex justify-between">
+          <button @click="reset" class="w-1/2 border-2 hover:bg-gray-100 border-neutral-950 py-2 rounded-l-md">
             Reset
+          </button>
+          <button @click="apply" class="w-1/2 bg-yellow-500 hover:bg-yellow-600 text-white py-2 rounded-r-md">
+            Terapkan
           </button>
         </div>
       </div>
@@ -34,6 +39,13 @@ export default {
     reset() {
       // Logika untuk mereset form
     },
+    close() {
+      this.$emit('close');
+    },
   },
 };
 </script>
+
+<style scoped>
+/* Additional styling if needed */
+</style>
